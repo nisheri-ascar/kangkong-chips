fed702195c39e2dc939070865f28cb40495c5f0f
nisheri-ascar <kertkert704@gmail.com>
2026-01-24 23:15:36 +0800

    implement bottom layer

--- grooved_bottle_test_4.py
+++ grooved_bottle_test_4.py
@@ -12,13 +12,14 @@ def bottle():
     num_slices = 4
     radius = 3
     groove_depth = 0.1
+    layer_cap = 0
     # Visual Attributes
     ambient = Color3(0,0,255)
     diffuse = 1.0
     specular = Color3(0,0,0)
     emmision = Color3(0,0,0) # wat the fuck is this again?
     shininess = 0.2
-    transparency = 1
+    transparency = 0.1 # NEVER SET TO NONFLOAT 1!!! FUCK YOU KARL
     for i in range(num_slices):
         # Alternate radius to make groove_depth
         
@@ -27,8 +28,18 @@ def bottle():
         print(f"iteration: {i}")
         print(f"r = {r}")
         print(f"z = {z}")
-        slice_cyl = Translated(0, 0, z, Cylinder(r, height / num_slices, 1))
+        print(f"layer_cap = {layer_cap}")
+
+        slice_cyl = Translated(0, 0, z, Cylinder(r, height / num_slices, layer_cap))
         scene += Shape(slice_cyl, Material(ambient, diffuse, specular, emmision, shininess, transparency))
+   
+       
+    scene += Shape(Cylinder(radius, 0.001, 1))
+
+
+
+
+
 bottle()
 Viewer.display(scene)
 
