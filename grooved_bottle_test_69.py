2ca095728c57b2df2293201d926af4e67a9518e5
nisheri-ascar <kertkert704@gmail.com>
2026-01-26 06:29:50 +0800

    works

--- grooved_bottle_test_69.py
+++ grooved_bottle_test_69.py
@@ -1,4 +1,4 @@
-print("Cylinders + Curved Polyline slopes!")
+print("Cylinders + Curved Polyline slopes with tweakable circle slices!")
 from openalea.plantgl.all import *
 import math
 
@@ -8,13 +8,15 @@ def bottle():
     global scene 
 
     # -------------------------
-    # Physical Attributes
+    # Tweakable parameters
     # -------------------------
     height = 2
     num_slices = 10
     radius = 3
     groove_depth = 0.2
-    circle_slices = 64
+    circle_slices = 64        # number of segments for circular appearance
+    transition_ratio = 0.3    # how tall the downslope is
+    num_pts_slope = 10        # smoothness of the curve
 
     # -------------------------
     # Visual Attributes
@@ -32,7 +34,6 @@ def bottle():
     # Slice + slope parameters
     # -------------------------
     slice_h = height / num_slices
-    transition_ratio = 0.3   # how tall the slope should be
     transition_h = slice_h * transition_ratio
     body_h = slice_h - transition_h
 
@@ -49,22 +50,24 @@ def bottle():
 
         # --- main cylinder ---
         cyl_h = slice_h
-        slice_cyl = Translated(0, 0, z, Cylinder(r0, cyl_h, 0, circle_slices))
+        slice_cyl = Translated(
+            0, 0, z,
+            Cylinder(r0, cyl_h, 0, circle_slices)  # use circle_slices here
+        )
         scene += Shape(slice_cyl, material)
 
         # --- curved polyline slope for groove ---
         if not is_last and r0 != r1:
-            num_pts = 10  # smoother curve
             pts = []
-            for j in range(num_pts + 1):
-                t = j / num_pts
-                # cosine easing curve for smooth slope
+            for j in range(num_pts_slope + 1):
+                t = j / num_pts_slope
+                # cosine easing for smooth slope
                 eased_t = (1 - math.cos(math.pi * t)) / 2
                 radius_at_t = r0 + eased_t * (r1 - r0)
                 height_at_t = z + body_h + eased_t * transition_h
                 pts.append(Vector2(radius_at_t, height_at_t))
             slope_polyline = Polyline2D(pts)
-            slope_revolved = Revolution(slope_polyline, circle_slices)
+            slope_revolved = Revolution(slope_polyline, slices=circle_slices)  # use circle_slices
             scene += Shape(slope_revolved, material)
 
     # -------------------------
